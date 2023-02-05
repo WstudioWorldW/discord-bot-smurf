f1b2e7addeaff6a234dd119bb932403139a22290
Savitov Daniel <danil.snip@gmail.com>
2023-02-05 19:53:15 +0500

    CommandsMessage

---
+++ apps/database/commands-message.js
@@ -0,0 +1,26 @@
+export const commandsMessage  = [
+    {
+        "name": "start",
+        "message": "hello user"
+    },
+    {
+        "name": "help",
+        "message": "Команда /start: стартует бота,\nКоманда /help вызывает подсказки,\nкоманда /rank выводит список рангов,\nКоманда /select помогает сделать выбор пользователю,\nКоманда /player запускает наш плеер,\nКоманда /game запускает игру."
+    },
+    {
+        "name": "rank",
+        "message": "command is empty"
+    },
+    {
+        "name": "select",
+        "message": "command is empty"
+    },
+    {
+        "name": "player",
+        "message": "command is empty"
+    },
+    {
+        "name": "game",
+        "message": "command is empty"
+    }
+]
--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-import { Client, GatewayIntentBits  } from 'discord.js';
+import { Client, GatewayIntentBits, Events } from 'discord.js';
 import { config, processStart } from './config.js';
+import {commandsMessage} from './apps/database/commands-message.js';
 
 
 class Bot {
@@ -17,5 +18,46 @@ class Bot {
     }
 }
 
-const bot = new Bot (new Client({ intents: [GatewayIntentBits.Guilds] }));
 
+class ClientCommandMessage extends Bot {
+    constructor (client, command) {
+        super(client);
+
+        this.client = this.client;
+
+        this.commandName = command.name;
+        this.message = command.message;
+
+        this.arrayCommands = this.commandName.length === this.message.length ? this.commandName.length : '';
+
+        this.sendMessageCommand(this.commandName, this.message, this.arrayCommands);
+    }
+    sendMessageCommand (name, message, arrayCommands) {
+        return this.client.on(Events.InteractionCreate, async interaction => {
+            if (!interaction.isChatInputCommand()) return;
+            
+            for (let i = 0; i < arrayCommands; i++) {
+                if (interaction.commandName === name[i]) {
+                    await interaction.reply(message[i]);
+                }
+            }
+        });
+    }
+}
+
+
+const bot = new Bot (new Client({ 
+    intents: [GatewayIntentBits.Guilds] 
+}));
+
+for (let i = 0; i < commandsMessage.length; i++) {
+    const clientCommandMessage = new ClientCommandMessage ((
+        new Client({ 
+            intents: [GatewayIntentBits.Guilds] 
+        })),
+        {
+            name: [commandsMessage[i].name],
+            message: [commandsMessage[i].message]
+        }
+    )
+}
